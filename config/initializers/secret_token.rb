b83820c1356d383cce5ec9cb58f2efe948704494
Kevin Kuchta <kevinmkuchta@gmail.com>
2013-06-30 15:23:30 -0700

    Base rails 4 install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nodegame::Application.config.secret_key_base = '9abe855db34b456fea3f9892d4020faf25d983f512afde5291805c85aff4380ec29f51e7baf049d574ea53b002a0a1222074adafa41a7483993fe58db21c158a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nodegame::Application.config.session_store :cookie_store, key: '_nodegame_session'
