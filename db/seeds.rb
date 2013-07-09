ba0737b5fa341153abe9c78643556b9970d09768
Kevin Kuchta <kevin@kevinkuchta.com>
2013-07-08 17:14:50 -0700

    Update seed file with useful test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,36 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+cabin = Node.create({
+  description:"You are inside a small cabin."
+})
+
+meadow = Node.create({
+  description:"You are in a small mountain meadow."
+})
+
+forest = Node.create({
+  description:"You are in a dark and scary forest"
+})
+
+cabin_door_out = Path.create({
+  start_node:cabin,
+  end_node:meadow,
+  state:'open',
+  start_description: "There is one door."
+})
+
+cabin_door_in = Path.create({
+  start_node:meadow,
+  end_node:cabin,
+  state:'open',
+  start_description: "There is a cabin with one door"
+})
+
+path_to_forest = Path.create({
+  start_node:meadow
+  end_node:forest,
+  state:'open',
+  start_description: "A faded path disappears into a forest to the north."
+
+
